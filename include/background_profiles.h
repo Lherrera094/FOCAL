202430a4ba2a657411c7d31518721fa8c1afbc56
Lherrera094 <herrera.woa94@gmail.com>
2024-05-27 17:35:03 +0200

    small changes

--- include/background_profiles.h
+++ include/background_profiles.h
@@ -1,6 +1,15 @@
 #ifndef BACKGROUND_PROFILES_H
 #define BACKGROUND_PROFILES_H
 
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "focal.h"
+#include "background_profiles.h"
+#include "hdf5.h"
+#include "grid_io.h"
+
 int make_density_profile( gridConfiguration *gridCfg, 
                           double cntrl_para, 
                           double n_e[gridCfg->Nx/2][gridCfg->Ny/2][gridCfg->Nz/2] );
