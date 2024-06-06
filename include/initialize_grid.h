982f4c52654e6056849a7c138ef86f25740ca0b4
Lherrera094 <herrera.woa94@gmail.com>
2024-06-06 13:20:55 -0600

    add antenna init

--- include/initialize_grid.h
+++ include/initialize_grid.h
@@ -19,6 +19,10 @@ typedef struct gridConfiguration {
         dx,dt;
 } gridConfiguration;
 
+typedef struct Grid {
+    
+} Grid;
+
 typedef struct beamConfiguration {
     int
         exc_signal,
@@ -43,6 +47,7 @@ typedef struct beamConfiguration {
 //functions in initialize grid
 int set2zero_1D( size_t N_x, double arr_1D[N_x] );
 int set2zero_3D( size_t N_x, size_t N_y, size_t N_z, double arr_3D[N_x][N_y][N_z] );
-void gridInit(gridConfiguration *gridCfg, int boundary);
+void gridConfInit(gridConfiguration *gridCfg, int boundary);
+void antenaInit(gridConfiguration *gridCfg, beamConfiguration *beamCfg);
 
 #endif
